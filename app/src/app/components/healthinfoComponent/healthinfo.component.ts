b67b1780b96f3825e1a2c19b26c27a6b39e2444e
prospecta <prospecta@prospecta.prospectatech.local>
2020-05-27 14:18:20 +0530

    7.0

--- app/src/app/components/healthinfoComponent/healthinfo.component.ts
+++ app/src/app/components/healthinfoComponent/healthinfo.component.ts
@@ -11,6 +11,8 @@ import { servicename } from 'app/sd-services/servicename';
 Legacy Service import Example :
 import { HeroService } from '../../services/hero/hero.service';
 */
+import { datasharingService } from 'app/services/datasharing/datasharing.service';
+
 import { masterdataService } from '../../services/masterdata/masterdata.service';
 
 @Component({
@@ -20,25 +22,28 @@ import { masterdataService } from '../../services/masterdata/masterdata.service'
 
 export class healthinfoComponent extends NBaseComponent implements OnInit {
 
-    constructor(private router: Router, private masterdata: masterdataService) {
+    constructor(private router: Router, private masterdata: masterdataService, private datasharingService:datasharingService) {
         super();
          let language = window.localStorage.getItem('language');
          this.localeService.language = language;
-         console.log(language);
+        //  console.log(language);
     }
     answer: string = '';
        shortTextOne = "had fever";
     shortTextTwo = "had Personal Contact";
+    showError:Boolean = false;
+    val1:any;
+    val2:any;
     ngOnInit() { }
 
-    onChangeRadio(e, questionIndex) {
+     onChangeRadio(e, questionIndex) {
         console.log('onChangeRadio called...')
         console.log("Question Index", questionIndex)
-        let val = e.value
-        this.answer = val
-        console.log("ths.ans", val);
+        this.val1 = e.value;
+        this.answer = this.val1;
+        console.log("ths.ans", this.val1);
         if (questionIndex == '1') {
-            this.masterdata.answer1 = val;
+            this.masterdata.answer1 = this.val1;
             this.masterdata.questionId = questionIndex;
             this.masterdata.shortTextOne = this.shortTextOne;
              localStorage.setItem('answer1', 
@@ -48,8 +53,15 @@ export class healthinfoComponent extends NBaseComponent implements OnInit {
                     "shortText": this.shortTextOne
                 }));
         }
+    }    onChangeRadioTwo(e, questionIndex) {
+        console.log('onChangeRadio2 called...')
+        console.log("Question Index", questionIndex)
+        this.val2 = e.value
+        this.answer = this.val2
+        console.log("ths.ans", this.val2);
+
         if (questionIndex == '2') {
-            this.masterdata.answer2 = val;
+            this.masterdata.answer2 = this.val2;
             this.masterdata.questionId2 = questionIndex;
             this.masterdata.shortTextTwo = this.shortTextTwo;
                localStorage.setItem('answer2', 
@@ -60,10 +72,17 @@ export class healthinfoComponent extends NBaseComponent implements OnInit {
                 })); }
 
     }
+
     onBack() {
         this.router.navigate(['/landingpage']);
     }
     onNext() {
-        this.router.navigate(['/hinfonext']);
+        console.log('val1',this.val1, 'val2',this.val2);
+        if(this.val1 && this.val2){
+            this.router.navigate(['/hinfonext']);
+        } else {
+        this.datasharingService.openSnackBar('Please answer for questions', 'X');
+        }
+       
     }
 }
