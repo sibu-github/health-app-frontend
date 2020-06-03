f5ab7e647a2253048601eaedea76190b842ae230
Sibaprasad Maiti <sibu.it13@gmail.com>
2020-06-04 01:44:39 +0530

    android-build.sh added

--- app/android-build.sh
+++ app/android-build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-rm -rf www platform plugins package-lock.json node_modules
-mkdir www
-cordova platform add android && npm run android-build
+echo "removing files"
+rm -rf www package-lock.json platforms dist node_modules
+echo "install dependency & create www folder"
 npm i && mkdir www
+echo "add android platform and build"
 cordova platform add android && npm run android-build
+
+
