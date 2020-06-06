782fded5b4f6422282cbd17b503a7a66c597780e
Sibaprasad Maiti <sibu.it13@gmail.com>
2020-06-06 18:30:15 +0530

    WIP

--- app/src/app/components/landingpageComponent/landingpage.component.ts
+++ app/src/app/components/landingpageComponent/landingpage.component.ts
@@ -53,8 +53,15 @@ export class landingpageComponent extends NBaseComponent implements OnInit {
   ) {
     super();
 
+    // bind callback function
     this.onLoadStartCallback = this.onLoadStartCallback.bind(this);
+    this.updateLocaleLanguage();
+    this.setIfMobileApp();
+    this.checkAccount();
+    this.subscribeToMsal();
+  }
 
+  updateLocaleLanguage() {
     // get the previously selected language from local storage
     // set the language if selected
     let language = window.localStorage.getItem("language");
@@ -64,7 +71,9 @@ export class landingpageComponent extends NBaseComponent implements OnInit {
     } else {
       this.defaultlang = "en";
     }
+  }
 
+  setIfMobileApp() {
     // check if opened from mobile app or from browser
     if (window["cordova"]) {
       this.isMobileApp = true;
@@ -105,9 +114,28 @@ export class landingpageComponent extends NBaseComponent implements OnInit {
   //when user selects lets starts button
   async letStart() {
     console.log("Lets Starts is working");
+
+    // when opened from browser
+    // for non employee flow the url contains the string /landpage
+    // route to personalinfo in that case
+    if (document.URL.indexOf("/landpage") > 0) {
+      this.router.navigate(["/personalinfo"]);
+      return;
+    }
+
+    this.showSpinner = true;
+
     if (this.isMobileApp) {
       this.letsStartMobile();
       return;
+    }
+
+    // when opened from browser check if already logged in
+    // otherwise show login page
+    if (this.isEmpLoggedIn) {
+      this.checkIfHRAdmn();
+    } else {
+      this.loginEmp();
     }
   }
 
@@ -162,20 +190,6 @@ export class landingpageComponent extends NBaseComponent implements OnInit {
     this.openInAppBrowser();
   }
 
-  // show the microsoft login window
-  showLoginScreen() {
-    // when opened from browser
-    // for non employee flow the url contains the string /landpage
-    // route to personalinfo in that case
-    if (document.URL.indexOf("/landpage") > 0) {
-      this.router.navigate(["/personalinfo"]);
-      return;
-    }
-  }
-
-  // show loginpopup for employee login
-  openLoginPopup() {}
-
   // open inappbrowser to show the login winoow
   openInAppBrowser() {
     const CLIENT_ID = this.systemService.getVal("azureClientID");
@@ -220,63 +234,66 @@ export class landingpageComponent extends NBaseComponent implements OnInit {
         return;
       }
 
-      // call service to get accessToken, refreshToken and user details
-      // set in the locastorage
-      let bh = await this.userService.getTokenFromCode(code);
-      this.setTokensNUserLocalStorage(bh);
-
-      // call service to check if the user is a hradmin or not
-      // if HRAdmin then we move to the optionpage
-      let email = window.localStorage.getItem("email");
-      // if email is not found in the localstorage then exit
-      // something wrong happened in this scenario which should never happen
-      if (!email || email === "undefined") {
-        this.showSpinner = false;
-        return;
-      }
-
-      bh = await this.hrmailService.verifyEmail(email);
-      if (
-        bh &&
-        bh.local &&
-        bh.local.result &&
-        bh.local.result.Authorized == "true"
-      ) {
-        this._zone.run(() => {
-          this.router.navigate(["/optionpage"]);
-        });
-        return;
-      }
-
-      // ------------------- FIX: 4261 ------------------
-      // check if user has submitted data for the day
-      // Note: for employee we check if the user has submitted data for the day already
-      // if yes we redirect to thank you page, otherwise redirect to landingpage
-      bh = await this.userService.getIfUserSubmitted(email);
-      console.log(bh);
-      let hasSubmitted = "no";
-      let colorCode = "green";
-      if (bh.local && bh.local.result) {
-        hasSubmitted = bh.local.result.updated;
-        colorCode = bh.local.result.colorCode;
-      }
-      // save the colorCode in localStorage
-      window.localStorage.setItem("colorCode", colorCode);
-      if (hasSubmitted === "yes" || hasSubmitted === "Yes") {
-        this._zone.run(() => {
-          this.router.navigate(["/thankyou"]);
-        });
-        return;
-      }
-
-      //If none of the above condition is true then we
-      // redirect to confirmDetails page as usual
-      this._zone.run(() => {
-        this.router.navigate(["/confirmdetails"]);
-      });
+      await this.getAccessTokenFromCode(code);
+      await this.checkIfHRAdmn();
     } catch (err) {
       console.error(err);
     }
+  }
+
+  async getAccessTokenFromCode(code) {
+    // call service to get accessToken, refreshToken and user details
+    // set in the locastorage
+    let bh = await this.userService.getTokenFromCode(code);
+    this.setTokensNUserLocalStorage(bh);
+  }
+
+  async checkIfHRAdmn() {
+    // call service to check if the user is a hradmin or not
+    // if HRAdmin then we move to the optionpage
+    let email = window.localStorage.getItem("email");
+    let bh = await this.hrmailService.verifyEmail(email);
+    if (
+      bh &&
+      bh.local &&
+      bh.local.result &&
+      bh.local.result.Authorized == "true"
+    ) {
+      this._zone.run(() => {
+        this.router.navigate(["/optionpage"]);
+      });
+      return;
+    }
+  }
+
+  async checkIfUserSubmittedData() {
+    // ------------------- FIX: 4261 ------------------
+    // check if user has submitted data for the day
+    // Note: for employee we check if the user has submitted data for the day already
+    // if yes we redirect to thank you page, otherwise redirect to landingpage
+    let email = window.localStorage.getItem("email");
+    let bh = await this.userService.getIfUserSubmitted(email);
+    console.log(bh);
+    let hasSubmitted = "no";
+    let colorCode = "green";
+    if (bh.local && bh.local.result) {
+      hasSubmitted = bh.local.result.updated;
+      colorCode = bh.local.result.colorCode;
+    }
+    // save the colorCode in localStorage
+    window.localStorage.setItem("colorCode", colorCode);
+    if (hasSubmitted === "yes" || hasSubmitted === "Yes") {
+      this._zone.run(() => {
+        this.router.navigate(["/thankyou"]);
+      });
+      return;
+    }
+
+    //If none of the above condition is true then we
+    // redirect to confirmDetails page as usual
+    this._zone.run(() => {
+      this.router.navigate(["/confirmdetails"]);
+    });
   }
 
   // set user details and tokens in localstorage
@@ -295,5 +312,49 @@ export class landingpageComponent extends NBaseComponent implements OnInit {
       ); // phone number received from AD contains space
       this.masterdata.email = bh.local.result.user.email;
     }
+  }
+
+  subscribeToMsal() {
+    this.broadcastService.subscribe("msal:loginSuccess", async (payload) => {
+      console.log("login success");
+      console.log(payload);
+
+      await this.getProfile();
+      await this.checkIfHRAdmn();
+    });
+  }
+
+  async checkAccount() {
+    console.log("check Account");
+    if (this.isMobileApp) {
+      return;
+    }
+
+    // when opened from browser
+    // for non employee flow the url contains the string /landpage
+    if (document.URL.indexOf("/landpage") > 0) {
+      return;
+    }
+
+    this.isEmpLoggedIn = !!this.authService.getAccount();
+  }
+
+  loginEmp() {
+    console.log("loginEmp");
+    this.authService.loginPopup();
+  }
+
+  async getProfile() {
+    console.log("getProfile");
+    const graphMeEndpoint = "https://graph.microsoft.com/v1.0/me";
+    const profile: any = await this.http.get(graphMeEndpoint).toPromise();
+
+    window.localStorage.setItem("email", profile.mail);
+    window.localStorage.setItem("username", profile.mail);
+    window.localStorage.setItem("firstName", profile.givenName);
+    window.localStorage.setItem("lastName", profile.surname);
+    window.localStorage.setItem("location", profile.officeLocation);
+    window.localStorage.setItem("phone", profile.mobilePhone.replace(" ", "")); // phone number received from AD contains space
+    this.masterdata.email = profile.mail;
   }
 }
