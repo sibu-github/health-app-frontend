121f9ce095db40bd2c151405576acbab6ce837ae
Sibaprasad Maiti <sibu.it13@gmail.com>
2020-06-11 01:26:55 +0530

    thankyou page copied from the previous commit

--- app/src/app/components/thankyouComponent/thankyou.component.ts
+++ app/src/app/components/thankyouComponent/thankyou.component.ts
@@ -1,9 +1,9 @@
 /*DEFAULT GENERATED TEMPLATE. DO NOT CHANGE SELECTOR TEMPLATE_URL AND CLASS NAME*/
 import { Component, OnInit } from "@angular/core";
 import { NBaseComponent } from "../../../../../app/baseClasses/nBase.component";
-import { HostListener } from '@angular/core';
-import { PlatformLocation } from '@angular/common';
-import {Router} from '@angular/router';
+import { HostListener } from "@angular/core";
+import { PlatformLocation } from "@angular/common";
+import { Router } from "@angular/router";
 import { NLocalStorageService } from "neutrinos-seed-services";
 /*
 Client Service import Example:
@@ -66,7 +66,11 @@ export class thankyouComponent extends NBaseComponent implements OnInit {
     return dayStr;
   }
 
-  constructor(location: PlatformLocation, private router: Router, private nLocalStorage: NLocalStorageService) {
+  constructor(
+    location: PlatformLocation,
+    private router: Router,
+    private nLocalStorage: NLocalStorageService
+  ) {
     super();
     // get the previously selected language from local storage
     // set the language if selected
@@ -81,31 +85,51 @@ export class thankyouComponent extends NBaseComponent implements OnInit {
   }
 
   ngOnInit() {
-
-    // for prepopulating the data
-    let uResp = this.nLocalStorage.getValue("userResponse");
-    if (typeof uResp === "string") {
-      this.localdata = JSON.parse(uResp);
-    } else {
-      this.localdata = uResp;
+    try {
+      // for prepopulating the data
+      let uResp = this.nLocalStorage.getValue("userResponse");
+      if (typeof uResp === "string") {
+        this.localdata = JSON.parse(uResp);
+      } else {
+        this.localdata = uResp;
+      }
+    } catch (err) {
+      console.error(err);
     }
 
-      // get firstName and lastName
-     let firstName =  this.nLocalStorage.getValue("firstName");
-     let lastName = this.nLocalStorage.getValue("lastName");
-     
-     if(firstName !== null && lastName !== null) {
-       console.log(firstName, lastName);
-         this.name = firstName + ' ' + lastName;
-          console.log(name);
-     } else {
-         if(this.localdata.firstName &&  this.localdata.lastName)   this.name = this.localdata.firstName + ' ' + this.localdata.lastName;
-     }
+    // get firstName and lastName
+    let firstName = this.nLocalStorage.getValue("firstName");
+    let lastName = this.nLocalStorage.getValue("lastName");
+
+    if (firstName !== null && lastName !== null) {
+      this.name = firstName + " " + lastName;
+    } else {
+      if (this.localdata.firstName && this.localdata.lastName)
+        this.name = this.localdata.firstName + " " + this.localdata.lastName;
+    }
     // set today
     // new addon time AM/PM
     var time = new Date();
-    console.log("time", time);
-    let timezone = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
-    this.today = this.getDay() + ' ' + timezone;
+    let timezone = time.toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
+    this.today = this.getDay() + " " + timezone;
   }
+  //press backbutton to exit app
+  //     document.addEventListener('deviceready', function() {
+
+  //     document.addEventListener("backbutton", ShowExitDialog, false);
+
+  // }, false);
+  // function ShowExitDialog() {
+  //         navigator.notification.confirm(
+  //                 ("Do you want to Exit?"), // message
+  //                 alertexit, // callback
+  //                 'My APp', // title
+  //                 'YES,NO' // buttonName
+  //         );
+
+  //     }
 }
