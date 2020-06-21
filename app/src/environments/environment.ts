91da7155c1fc5e1686e74d0d6e19b5fb4645fcba
Sibaprasad Maiti <sibu.it13@gmail.com>
2020-06-21 22:18:47 +0530

    ssdURL and clientID modified to deploy code in UAT

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     "name": "dev",
     "properties": {
         "production": false,
-        "ssdURL": "https://health-appprod.azurewebsites.net",
+        "ssdURL": "https://health-appdev.azurewebsites.net",
         "tenantName": "blucocoondigital",
         "appName": "health-app",
         "namespace": "com.blucocoondigital.health-app",
@@ -16,7 +16,7 @@ export const environment = {
         "azureScopes": "openid profile offline_access",
         "azureResponseType": "code",
         "azureAuthority": "https://login.microsoftonline.com/8d88c9c2-2058-486d-9cd4-2fc9010326bc",
-        "clientIdWebLogin": "a88cc2a4-ee56-4244-aa32-153fe32ba9fc",
-        "redirectUrlWebLogin": "https://carefirst-appprod.azurewebsites.net/logincomplete"
+        "clientIdWebLogin": "c4f2534b-88d8-4671-9804-495b19e235aa",
+        "redirectUrlWebLogin": "https://health-appuat.azurewebsites.net/logincomplete"
     }
 }
