81f3f0bef147ea77807545a062ac0ff4440a5d74
Sibaprasad Maiti <sibu.it13@gmail.com>
2020-06-30 12:15:54 +0530

    authService.logout added in logout service

--- app/src/app/services/logout/logout.service.ts
+++ app/src/app/services/logout/logout.service.ts
@@ -2,13 +2,14 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { NLocalStorageService } from "neutrinos-seed-services";
-
+import {  MsalService } from "@azure/msal-angular";
 
 @Injectable()
 export class logoutService {
-    constructor(private router: Router, private nLocalStorage: NLocalStorageService) { }
+    constructor(private router: Router, private nLocalStorage: NLocalStorageService,private authService: MsalService,) { }
 
     logout() {
+        this.authService.logout();
         this.nLocalStorage.clearLocalStorage();
         this.router.navigate(['/landingpage']);
     }
