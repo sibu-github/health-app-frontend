60f1e9c8ea9819cd2de61c443fc3d3f59712a199
Sibaprasad Maiti <sibu.it13@gmail.com>
2020-07-02 22:30:32 +0530

    ios build for prod

--- app/android-build.sh
+++ app/android-build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# 
+# Few things to take care for android production build
+# 1. version in package.json
+# 2. before generating the apk from android studio
+#  make sure to comment out the read_phone_state permission
+# in android-manifest.xml file
+# 
+# 
+
+
+
+
 echo "removing files"
 rm -rf www package-lock.json platforms dist node_modules
 echo "install dependency & create www folder"
