bcb97c937b869a5fab631dc3569e85a30748ad4f
Sibaprasad Maiti <sibu.it13@gmail.com>
2020-07-08 23:04:31 +0530

    prod url modified in environment

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     "name": "prod",
     "properties": {
         "production": "false",
-        "ssdURL": "https://health-appdev.azurewebsites.net",
+        "ssdURL": "https://health-appprod.azurewebsites.net",
         "tenantName": "blucocoondigital",
         "appName": "health-app",
         "namespace": "com.blucocoondigital.health-app",
@@ -16,8 +16,8 @@ export const environment = {
         "azureScopes": "openid profile offline_access",
         "azureResponseType": "code",
         "azureAuthority": "https://login.microsoftonline.com/8d88c9c2-2058-486d-9cd4-2fc9010326bc",
-        "clientIdWebLogin": "c4f2534b-88d8-4671-9804-495b19e235aa",
-        "redirectUrlWebLogin": "https://health-appuat.azurewebsites.net/logincomplete",
-        "postLogoutRedirectUri": "https://health-appuat.azurewebsites.net/landingpage"
+        "clientIdWebLogin": "a88cc2a4-ee56-4244-aa32-153fe32ba9fc",
+        "redirectUrlWebLogin": "https://carefirst-appprod.azurewebsites.net/logincomplete",
+        "postLogoutRedirectUri": "https://carefirst-appprod.azurewebsites.net/landingpage"
     }
 }
