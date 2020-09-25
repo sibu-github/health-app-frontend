1e4af7a82e6971402841ee1f109aa85ed167ac6e
Sibaprasad Maiti <sibu.it13@gmail.com>
2020-09-25 10:14:00 +0530

    environment modified

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -1,23 +1,27 @@
 export const environment = {
-    "name": "dev",
-    "properties": {
-        "production": "true",
-        "ssdURL": "https://health-appdev.azurewebsites.net",
-        "tenantName": "blucocoondigital",
-        "appName": "health-app",
-        "namespace": "com.blucocoondigital.health-app",
-        "googleMapKey": "AIzaSyCSTnVwijjv0CFRA4MEeS-H6PAQc87LEoU",
-        "useDefaultExceptionUI": true,
-        "isIDSEnabled": "false",
-        "webAppMountpoint": "web",
-        "azureClientID": "c4f2534b-88d8-4671-9804-495b19e235aa",
-        "azureAuthURL": "https://login.microsoftonline.com/8d88c9c2-2058-486d-9cd4-2fc9010326bc/oauth2/v2.0/authorize",
-        "redirectURL": "https://health-appuat.azurewebsites.net/logincomplete",
-        "azureScopes": "openid profile offline_access",
-        "azureResponseType": "code",
-        "azureAuthority": "https://login.microsoftonline.com/8d88c9c2-2058-486d-9cd4-2fc9010326bc",
-        "clientIdWebLogin": "c4f2534b-88d8-4671-9804-495b19e235aa",
-        "redirectUrlWebLogin": "https://health-appuat.azurewebsites.net/logincomplete",
-        "postLogoutRedirectUri": "https://health-appuat.azurewebsites.net/landingpage"
-    }
-}
+  name: "dev",
+  properties: {
+    production: "true",
+    ssdURL: "https://health-appprod.azurewebsites.net",
+    tenantName: "blucocoondigital",
+    appName: "health-app",
+    namespace: "com.blucocoondigital.health-app",
+    googleMapKey: "AIzaSyCSTnVwijjv0CFRA4MEeS-H6PAQc87LEoU",
+    useDefaultExceptionUI: true,
+    isIDSEnabled: "false",
+    webAppMountpoint: "web",
+    azureClientID: "c4f2534b-88d8-4671-9804-495b19e235aa",
+    azureAuthURL:
+      "https://login.microsoftonline.com/8d88c9c2-2058-486d-9cd4-2fc9010326bc/oauth2/v2.0/authorize",
+    redirectURL: "https://health-appuat.azurewebsites.net/logincomplete",
+    azureScopes: "openid profile offline_access",
+    azureResponseType: "code",
+    azureAuthority:
+      "https://login.microsoftonline.com/8d88c9c2-2058-486d-9cd4-2fc9010326bc",
+    clientIdWebLogin: "a88cc2a4-ee56-4244-aa32-153fe32ba9fc",
+    redirectUrlWebLogin:
+      "https://carefirst-appprod.azurewebsites.net/logincomplete",
+    postLogoutRedirectUri:
+      "https://carefirst-appprod.azurewebsites.net/landingpage",
+  },
+};
